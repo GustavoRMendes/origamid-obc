d253eff5cc596e215059281b87f9a1ae221dcd93
Gustavo Mendes <gustavormendess@gmail.com>
2024-08-05 11:44:41 -0400

    adicionar conteúdo sobre booleanos e condicionais

--- javascript/boolean-e-condicionais/main.js
+++ javascript/boolean-e-condicionais/main.js
@@ -1,1 +1,33 @@
 // Boolean e Condicionais
+
+const verdadeiro = true;
+const falso = false;
+
+if (true) {
+  console.log(true);
+} else if (false) {
+  console.log(false);
+} else {
+  console.log("else");
+}
+
+if (verdadeiro) {
+  console.log("true");
+} else if (falso) {
+  console.log("falso");
+} else {
+  console.log("nenhum");
+}
+
+console.log(10 === 20);
+console.log(10 >= 20);
+console.log(10 <= 20);
+console.log(10 !== 20);
+console.log(!false);
+console.log(!!verdadeiro);
+console.log(!!falso);
+console.log(verdadeiro || falso);
+console.log(verdadeiro && falso);
+const resultado =
+  verdadeiro === "falso" ? "é falso" : "verdadeiro é verdadeiro";
+console.log(resultado);
