2128cb5a9ea9621923fe4714645672cbd2049a2b
Gustavo Mendes <gustavormendess@gmail.com>
2024-08-05 10:03:04 -0400

    adicionar conteúdo sobre os operadores e números

--- javascript/operadores/main.js
+++ javascript/operadores/main.js
@@ -1,1 +1,26 @@
 // Operadores e Expressões
+const soma = 1 + 1;
+const subtracao = 1 - 1;
+const multiplicacao = 1 - 1;
+const divisao = 1 / 1;
+const resto = 1 % 1;
+const expoente = 1 ** 1;
+const e = 2e10;
+
+// agrupamento
+const agrupamento = (5 + 5) / 2;
+
+// atribuição
+const atribuicao = 2;
+atribuicao += 4;
+
+// incremento e decremento
+const numero = 5;
+numero++;
+--numero;
+
+// transformar em número
+const quantidade = "20";
+console.log(+quantidade);
+
+// a ordem importa * / + -
