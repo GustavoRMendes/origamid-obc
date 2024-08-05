388513c77a0540af036747d43924d77a03d88972
Gustavo Mendes <gustavormendess@gmail.com>
2024-08-05 09:28:31 -0400

    log com os tipos usando typeof

--- javascript/tipos-de-dados/main.js
+++ javascript/tipos-de-dados/main.js
@@ -7,3 +7,13 @@ const object = {};
 const symbol = Symbol();
 const indefinido = undefined;
 const nulo = null;
+
+// typeof
+console.log(typeof texto);
+console.log(typeof numero);
+console.log(typeof booleano);
+console.log(typeof symbol);
+console.log(typeof indefinido);
+console.log(typeof nulo);
+console.log(typeof object);
+console.log(typeof bigint);
