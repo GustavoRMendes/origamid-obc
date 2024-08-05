005b2079c22c73b18a65b05fad213c56877ac15d
Gustavo Mendes <gustavormendess@gmail.com>
2024-08-05 10:22:59 -0400

    adicionar exercício sobre os recursos do navegador

---
+++ javascript/recursos-do-navegador/exercicio-1/script.js
@@ -0,0 +1,11 @@
+let nome = prompt("Digite seu nome: ");
+let sobrenome = prompt("Digite seu sobrenome: ");
+let campoDeEstudo = prompt("Digite seu campo de estudo: ");
+let anoDeNasc = prompt("Digite seu ano de nascimento: ");
+
+alert(`
+  Nome do recruta: ${nome}
+  Sobrenome do recruta: ${sobrenome}
+  Campo de estudo do recruta: ${campoDeEstudo}
+  Ano de nascimento do recruta: ${anoDeNasc}
+`);
