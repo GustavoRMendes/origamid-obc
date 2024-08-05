980dc914bf894ca7ef7f0544bdb0c02cd41b6e4b
Gustavo Mendes <gustavormendess@gmail.com>
2024-08-05 10:16:51 -0400

    adicionar conteúdo sobre os recursos do navegador

---
+++ javascript/recursos-do-navegador/main.js
@@ -0,0 +1,12 @@
+// Recursos do navegador
+
+// window
+window.alert("Mensagem");
+window.confirm("Confirma essa mensagem?");
+window.prompt("Digite seu nome:");
+
+// console
+console.log("log");
+console.warn("warn");
+console.error("error");
+console.table("table");
