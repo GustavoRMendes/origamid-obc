c1c63322aa1a87d40460f0f45bbf90c64402af71
Gustavo Mendes <gustavormendess@gmail.com>
2024-08-06 22:24:38 -0400

    adicionar exercício boolean e condicionais

--- javascript/boolean-e-condicionais/exercicio1/index.js
+++ javascript/boolean-e-condicionais/exercicio1/index.js
@@ -4,3 +4,11 @@ if (idade >= 18) {
 } else {
   return "Você é menor de idade.";
 }
+let velocidade = 70;
+if (velocidade >= 100) {
+  return "Diminua sua velocidade...";
+} else if (velocidade === 50) {
+  return "Velocidade média.";
+} else {
+  return "Velocidade boa.";
+}
