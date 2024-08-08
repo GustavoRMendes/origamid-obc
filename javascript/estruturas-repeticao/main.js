3b8963abe348f237770dbf916feaacd50a56e9e6
Gustavo Mendes <gustavormendess@gmail.com>
2024-08-08 18:21:47 -0400

    organizar documento com comentários

--- javascript/estruturas-repeticao/main.js
+++ javascript/estruturas-repeticao/main.js
@@ -1,4 +1,4 @@
-// For, While, Do While
+// For
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 for (let i = 0; i < numbers.length; i++) {
   console.log(numbers[i]);
@@ -6,10 +6,14 @@ for (let i = 0; i < numbers.length; i++) {
 for (let cont = 0; cont <= 10; cont++) {
   console.log(cont);
 }
+
+// While
 const condicao = true;
 while (condicao) {
   console.log("Enquanto a condição for verdadeira.");
 }
+
+// Do While
 // Executado pelo menos uma vez
 do {
   console.log("Faça enquanto a condição for falsa");
