940db6f447a817c9073c3788d2c80823792b2ebd
Gustavo Mendes <gustavormendess@gmail.com>
2024-08-19 10:17:35 -0400

    adicionar função map para mapear um array de nomes

--- javascript/functions/high-order-function/main.js
+++ javascript/functions/high-order-function/main.js
@@ -16,3 +16,19 @@ const lista = [1, 2, 3, 4, 5, 6, 7];
 lista.forEach(function (x) {
   console.log(x);
 });
+
+const personagens = [
+  { nivel: 42, nome: "Thrall", raca: "Orc", classe: "Xamã" },
+  { nivel: 28, nome: "Garrosh", raca: "Orc", classe: "Guerreiro" },
+  { nivel: 35, nome: "Varok", raca: "Orc", classe: "Guerreiro" },
+  { nivel: 35, nome: "Uther", raca: "Humano", classe: "Paladino" },
+  { nivel: 26, nome: "Jaina", raca: "Humano", classe: "Maga" },
+  { nivel: 39, nome: "Tyrande", raca: "Elfo Noturno", classe: "Sacerdotisa" },
+  { nivel: 29, nome: "Muradin", raca: "Anão", classe: "Guerreiro" },
+];
+const listaDeNomes = [];
+personagens.map((pessoa) => {
+  listaDeNomes.push(pessoa.nome);
+});
+
+console.log(listaDeNomes);
