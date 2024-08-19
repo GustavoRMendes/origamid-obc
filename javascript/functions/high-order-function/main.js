b23490b098076985d5cbf88683415faa17a5f1e9
Gustavo Mendes <gustavormendess@gmail.com>
2024-08-19 10:08:12 -0400

    high order function

--- javascript/functions/high-order-function/main.js
+++ javascript/functions/high-order-function/main.js
@@ -1,1 +1,18 @@
-// High Order Fuctions
+// High Order Fuctions -> Passa outra função como parâmetro
+
+function calcular(x, y, operacao) {
+  const resultado = operacao(x, y);
+  return resultado;
+}
+
+function somar(x, y) {
+  return x + y;
+}
+
+console.log(calcular(19, 20, somar));
+
+const lista = [1, 2, 3, 4, 5, 6, 7];
+
+lista.forEach(function (x) {
+  console.log(x);
+});
