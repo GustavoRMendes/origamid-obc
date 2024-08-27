2252f43ea5e62608d87c61279f45a46b7b29e9fa
Gustavo Mendes <gustavormendess@gmail.com>
2024-08-27 15:38:26 -0400

    add array.sort

--- javascript/functions/high-order-function/main.js
+++ javascript/functions/high-order-function/main.js
@@ -12,7 +12,7 @@ function somar(x, y) {
 console.log(calcular(19, 20, somar));
 
 const lista = [1, 2, 3, 4, 5, 6, 7];
-
+// forEach
 lista.forEach(function (x) {
   console.log(x);
 });
@@ -26,21 +26,35 @@ const personagens = [
   { nivel: 39, nome: "Tyrande", raca: "Elfo Noturno", classe: "Sacerdotisa" },
   { nivel: 29, nome: "Muradin", raca: "Anão", classe: "Guerreiro" },
 ];
+// map
 const listaDeNomes = [];
 personagens.map((pessoa) => {
   listaDeNomes.push(pessoa.nome);
 });
 
 console.log(listaDeNomes);
-
+// filter
 const filtro = personagens.filter((person) => {
   return person.nivel > 30;
 });
 
 console.log(filtro);
-
+// reduce
 const reduzir = personagens.reduce((acc, valor) => {
   return acc + valor.nivel;
 }, 0);
 
 console.log(reduzir);
+
+// sort -> Altera o array original, se não quisermos modificar usamos array.slice().sort(() => {})
+
+const decrescente = [10, 5, 83, 29, 4, 2];
+const crescente = [10, 3, 5, 1, 0];
+decrescente.sort((a, b) => {
+  return b - a;
+});
+console.log(decrescente);
+crescente.sort((a, b) => {
+  return a - b;
+});
+console.log(crescente);
